957d2587284ae0655a9383e6bb67c22797d5d43f
Monuwithgithub <148701712+Monuwithgithub@users.noreply.github.com>
2026-03-18 01:54:40 +0530

    Time: 764 ms (59.32%), Space: 0B (100%) - LeetHub

---
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -0,0 +1,10 @@
+SELECT P.product_id,
+       ROUND(
+           COALESCE(SUM(P.price * U.units) / SUM(U.units), 0), 
+       2) AS average_price
+FROM Prices P
+LEFT JOIN UnitsSold U 
+  ON P.product_id = U.product_id
+ AND P.start_date <= U.purchase_date
+ AND U.purchase_date <= P.end_date
+GROUP BY P.product_id;
