4c168a5562388bd45154c578c7ea40e411872a64
Monuwithgithub <148701712+Monuwithgithub@users.noreply.github.com>
2026-03-19 22:10:02 +0530

    Time: 3 ms (87.45%), Space: 43.1 MB (82.52%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            // If opening bracket → push
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } 
+            // If closing bracket
+            else {
+                // If stack empty → invalid
+                if (stack.isEmpty()) return false;
+
+                char top = stack.pop();
+
+                // Check matching
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        // If stack empty → valid
+        return stack.isEmpty();
+    }
+}
