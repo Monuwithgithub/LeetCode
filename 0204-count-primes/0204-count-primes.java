be83f42c0621126b0d188bb6c93d91aa67782fa4
Monuwithgithub <148701712+Monuwithgithub@users.noreply.github.com>
2026-04-15 23:57:49 +0530

    Time: 107 ms (27.95%), Space: 51 MB (77.16%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int countPrimes(int n) {
+        
+        if (n <= 2) return 0;
+        
+        boolean[] isPrime = new boolean[n];
+        
+        // Assume all are prime initially
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+        
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                
+                // Mark multiples as non-prime
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        
+        // Count primes
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) count++;
+        }
+        
+        return count;
+    }
+}
